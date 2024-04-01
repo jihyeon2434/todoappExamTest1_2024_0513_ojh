52fc6ea0c61761ce7e93a132743b6da160c438fd
JUNGmoon123 <jungpro567@gmail.com>
2024-04-01 17:03:30 +0900

    tailwind, mui 충돌 피하는 방법

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
+'use client';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        안녕하세요
-      </div>
-    </main>
+    <>
+      <Button className="tw-bg-red-500" variant="contained">
+        Contained
+      </Button>
+    </>
   );
 }
