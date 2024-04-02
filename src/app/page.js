60e088a477a58fb5662586918bdbed697ef3e1f3
JUNGmoon123 <jungpro567@gmail.com>
2024-04-02 15:31:26 +0900

    MUI taps적용구현했음

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,62 @@
 'use client';
 
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { ThemeProvider } from '@mui/material/styles';
-import { Button, AppBar, Toolbar, Drawer, List, ListItemButton } from '@mui/material';
+import {
+  Button,
+  AppBar,
+  Toolbar,
+  Drawer,
+  List,
+  ListItemButton,
+  Tab,
+  Tabs,
+  Typography,
+  Box,
+} from '@mui/material';
 import theme from './theme';
 import { FaBars } from 'react-icons/fa';
 import Link from 'next/link';
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
 
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}>
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 export default function App() {
   const [open, setOpen] = React.useState(false);
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <>
@@ -29,6 +77,24 @@ export default function App() {
           </Toolbar>
         </AppBar>
         <Toolbar />
+        <Box sx={{ width: '100%' }} className="tw-justify-center">
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Item One" {...a11yProps(0)} />
+              <Tab label="Item Two" {...a11yProps(1)} />
+              <Tab label="Item Three" {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            Item One
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            Item Three
+          </CustomTabPanel>
+        </Box>
         <section className="tw-h-screen tw-flex tw-items-center tw-justify-center tw-text-[5rem]">
           section
         </section>
