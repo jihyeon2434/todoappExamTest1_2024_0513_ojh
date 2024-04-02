cfa7e2e2a7a87d176e3428e6a7ae15c9491f5b27
JUNGmoon123 <jungpro567@gmail.com>
2024-04-02 11:15:02 +0900

    .eslintrc.json

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 'use client';
-import Button from '@mui/material/Button';
 
-export default function Home() {
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { Button, Box, AppBar, Toolbar } from '@mui/material';
+import theme from './theme';
+//theme.js가져오기
+import { MdDeleteForever } from "react-icons/md";
+
+export default function App() {
   return (
     <>
-      <Button className="tw-bg-red-500" variant="contained">
-        Contained
-      </Button>
+      <ThemeProvider theme={theme}>
+        <Button variant="contained" href='sub/'>버튼</Button>
+        <Button variant="contained" startIcon={<MdDeleteForever />}>?</Button>
+        
+      </ThemeProvider>
     </>
   );
 }
